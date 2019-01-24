c9a776258be4bd846964f61c5e0dfd67fe80c175
summerforver <122044024@qq.com>
2019-01-24 10:30:11 +0800

    Initial Commit

---
+++ English/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  English
+//
+//  Created by 王一卓 on 2019/1/24.
+//  Copyright © 2019年 王一卓. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ English/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  English
+//
+//  Created by 王一卓 on 2019/1/24.
+//  Copyright © 2019年 王一卓. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
